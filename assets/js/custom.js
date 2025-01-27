6b798fbf0daebeec9e22d8bb393e95ec3f5f2090
bdruhul <wp.dev.bdruhul@gmail.com>
2025-01-27 23:31:07 +0600

    challenge-3

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+// tailwind custom css
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          'primary-color':'#E76F51',
+          'dark-1':'#000',
+        }
+      }
+    }
+  }
+// tailwind custom css
